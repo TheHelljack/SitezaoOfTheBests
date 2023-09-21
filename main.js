742c35a3cb159d5acd95795252601e1a16fabe75
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2023-09-21 08:20:25 -0300

    a

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ let Contador = 0;
 
 
 while (Contador <lista.length) {
-    lista[Contador].onclick = function ();
-    Toca ('som_tecla_toim');
+    lista[Contador].onclick = function ()
+    {Toca ('som_tecla_toim');
+}
     const classe = lista[Contador].classList[1]
     console.log(classe);
     Contador = Contador + 1;
