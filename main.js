fc6bde0e973e3c93420d2015ac72c9d7a46e785c
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2023-09-21 07:33:59 -0300

    a

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
-While (Contador <9) {
+while (Contador <9) {
     lista[Contador].onclick = Toca;
     Contador = Contador + 1;
     console.log(Contador);
