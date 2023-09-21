a97c2ca354e1ddfddd3e9418741f33e90dd3f919
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2023-09-21 08:03:39 -0300

    a

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
+
 while (Contador <9) {
+    const classe = lista[Contador].classList[1]
+    console.log(classe);
     lista[Contador].onclick = Toca;
     Contador = Contador + 1;
     console.log(Contador);
