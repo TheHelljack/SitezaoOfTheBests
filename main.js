9ba11aed0eddd4af5666abb7a2be8edfdc858e40
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2023-09-29 09:36:18 -0300

    a

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let Contador = 0;
 
 while (Contador <lista.length) {
     lista[Contador].onclick = function (){
-        tocar('idAudio');
+        tocar(idAudio);
 }
     const classe = lista[Contador].classList[1]
     console.log(classe);
