3025de4035b1d5047e6ef7a394e93a18b980c464
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2023-11-13 11:44:43 -0300

    a

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ function tocar(idElementoAudio) {
 }
 const lista = document.querySelectorAll('.tecla');
 
-let Contador = 0;
-
-while (Contador <lista.length) {
+let (Contador = 0; (Contador <lista.length); Contador++) {
     const tecla = lista[Contador]
     const classe = tecla.classList[1]
     const idAudio = `#som_${classe}`;
